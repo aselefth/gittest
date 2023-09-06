1b881831a31322367aad5a857a7fd582daf300af
sanokkk <sashaevstr2002@yandex.ru>
2023-09-06 12:43:17 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	s := []byte("hello")
+	w.Write(s)
+	w.WriteHeader(201)
+
+}
+
+func main() {
+	fmt.Println("running server")
+	http.HandleFunc("/", helloWorld)
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		os.Exit(1)
+	}
+
+}
